5c812f0bcafbf905c4de59b7f98a7c06f469c7a6
bryandaniel1 <bryandaniel2003@yahoo.com>
2020-06-06 19:47:13 -0400

    Updated pom

--- netbeans/comparehospitalcharges/src/main/java/com/daniel/hospitalcharges/managedbean/MapBean.java
+++ netbeans/comparehospitalcharges/src/main/java/com/daniel/hospitalcharges/managedbean/MapBean.java
@@ -1,8 +1,8 @@
 package com.daniel.hospitalcharges.managedbean;
 
-import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import org.apache.commons.lang3.text.WordUtils;
 
 /**
